a6b4fbe35319787e1314bf477923002a7eeef216
UndChain-Chris <undchain@gmail.com>
2025-11-26 17:02:28 -0500

    Fix rustls 0.21 API compatibility - simplify imports and trait implementation
    - Change imports from rustls::client::danger to rustls::client
    
    - Use Certificate instead of CertificateDer
    
    - Simplify ServerCertVerifier trait to rustls 0.21 API (no extra methods)
    
    - Use with_safe_defaults() and with_custom_certificate_verifier() for builder

--- src/webrtc_link.rs
+++ src/webrtc_link.rs
@@ -2,9 +2,8 @@ use bytes::Bytes;
 use futures_util::stream::{SplitSink, SplitStream};
 use futures_util::{SinkExt, StreamExt};
 use log::{debug, error, info, warn};
-use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
-use rustls::pki_types::{CertificateDer, ServerName};
-use rustls::{ClientConfig, DigitallySignedStruct, RootCertStore, SignatureScheme};
+use rustls::client::{ServerCertVerified, ServerCertVerifier};
+use rustls::{Certificate, ClientConfig, RootCertStore, ServerName};
 use serde::{Deserialize, Serialize};
 use std::pin::Pin;
 use std::sync::Arc;
@@ -49,47 +48,14 @@ fn insecure_verifier() -> Arc<dyn ServerCertVerifier> {
     impl ServerCertVerifier for InsecureVerifier {
         fn verify_server_cert(
             &self,
-            _end_entity: &CertificateDer<'_>,
-            _intermediates: &[CertificateDer<'_>],
-            _server_name: &ServerName<'_>,
-            _ocsp: &[u8],
-            _now: rustls::pki_types::UnixTime,
+            _end_entity: &Certificate,
+            _intermediates: &[Certificate],
+            _server_name: &ServerName,
+            _scts: &mut dyn Iterator<Item = &[u8]>,
+            _ocsp_response: &[u8],
+            _now: std::time::SystemTime,
         ) -> Result<ServerCertVerified, rustls::Error> {
             Ok(ServerCertVerified::assertion())
-        }
-
-        fn verify_tls12_signature(
-            &self,
-            _message: &[u8],
-            _cert: &CertificateDer<'_>,
-            _dss: &DigitallySignedStruct,
-        ) -> Result<HandshakeSignatureValid, rustls::Error> {
-            Ok(HandshakeSignatureValid::assertion())
-        }
-
-        fn verify_tls13_signature(
-            &self,
-            _message: &[u8],
-            _cert: &CertificateDer<'_>,
-            _dss: &DigitallySignedStruct,
-        ) -> Result<HandshakeSignatureValid, rustls::Error> {
-            Ok(HandshakeSignatureValid::assertion())
-        }
-
-        fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
-            vec![
-                SignatureScheme::RSA_PKCS1_SHA256,
-                SignatureScheme::RSA_PKCS1_SHA384,
-                SignatureScheme::RSA_PKCS1_SHA512,
-                SignatureScheme::ECDSA_NISTP256_SHA256,
-                SignatureScheme::ECDSA_NISTP384_SHA384,
-                SignatureScheme::ECDSA_NISTP521_SHA512,
-                SignatureScheme::RSA_PSS_SHA256,
-                SignatureScheme::RSA_PSS_SHA384,
-                SignatureScheme::RSA_PSS_SHA512,
-                SignatureScheme::ED25519,
-                SignatureScheme::ED448,
-            ]
         }
     }
 
@@ -182,10 +148,10 @@ impl WebRtcLink {
 
         let connector: Option<Connector> = if self.allow_skip_cert_check {
             let config = ClientConfig::builder()
+                .with_safe_defaults()
                 .with_root_certificates(RootCertStore::empty())
                 .with_no_client_auth()
-                .dangerous()
-                .set_certificate_verifier(insecure_verifier());
+                .with_custom_certificate_verifier(insecure_verifier());
             Some(Connector::Rustls(Arc::new(config)))
         } else {
             None
